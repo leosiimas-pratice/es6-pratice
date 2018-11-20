0407c16ef3a5c0fa8864365401ff5355b8746c4e
Leonardo Abreu <abreu.leo@hotmail.com>
2018-11-20 00:32:06 -0200

    fix problem hooks

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
-module.exports = {
-    sum(num1,num2){return num1 + num2},
-    sub(num1,num2){return num1 - num2},
-    mul(num1,num2){return num1 * num2},
-    div(num1,num2){
-        return (num2  == 0) ? "Não é possivel divisão por zero!" : num1 / num2
-
-    }
+export function sum(num1, num2) { return num1 + num2; }
+export function sub(num1, num2) { return num1 - num2; }
+export function mul(num1, num2) { return num1 * num2; }
+export function div(num1, num2) {
+  return (num2 === 0) ? 'Não é possivel divisão por zero!' : num1 / num2;
 }
